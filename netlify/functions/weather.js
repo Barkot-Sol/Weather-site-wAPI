5979ac747b901a912efce26287ff712b3acc3b2f
Barkot Solomon <barkotsolomon5@gmail.com>
2026-05-12 23:51:58 +0300

    update Weather.js

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     const city = event.queryStringParameters.city;
-    const apiKey = process.env.API_KEY; // safely stored in Netlify
+    const apiKey = process.env.API_KEY; 
 
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
     const data = await res.json();
